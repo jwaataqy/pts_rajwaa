7b82a9d1960a3487ad7c9b2cfaba887201ba20ef
MuhammadRajwaaAtaqiy <muhammadrajwaataqiy@gmail.com>
2023-03-17 10:46:31 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.{html,js}','./index.html'],
+  theme: {
+    extend: {
+      background:'url(../assetuts/gambar.png)',
+      background1:'url(../assetuts/logo.png)',
+      
+    },
+  },
+  plugins: [],
+}
